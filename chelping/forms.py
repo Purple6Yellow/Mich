f385e0291572c34c6911b1fdcd3ba78f5aab884e
Purple6yellow <vikamper@hotmail.com>
2025-07-01 20:57:08 +0200

    1 juli

--- chelping/forms.py
+++ chelping/forms.py
@@ -3,16 +3,41 @@ from django.forms import ModelForm
 from .models import Contact
 
 ########## CONTACT FORMULIER #############
+NESTED_LAND = [
+    ('Basisschool', [
+        ('0','groep 3'),
+        ('1','groep 4'),
+        ('2','groep 5'),
+        ('3','groep 6'),
+        ('4','groep 7'),
+        ('5','groep 8'),
+]),
+    ('Vmbo', [
+        ('0','basis'),
+        ('1','kader'),
+        ('2','mavo'),
+]),
+    ('Havo', [
+        ('0','1e klas'),
+        ('1','2e klas'),
+        ('2','3e klas'),
+]),
+    ('Vwo', [
+        ('0','1e klas'),
+        ('1','2e klas'),
+]),
+    ]
+
 class Form_Co (forms.ModelForm):
+    niveau = forms.ChoiceField(choices = NESTED_LAND)
+
     class Meta:
         model = Contact
-        fields = '__all__'
+        fields = ['naam', 'leeftijd', 'niveau', 'email','telefoon', 'training']
         labels = {  
             'naam':'',
             'leeftijd':'',}
         widgets = {
-               'naam': forms.TextInput (attrs={'class': 'form-controle', 'placeholder': 'optioneel',}),
-               'leeftijd': forms.TextInput (attrs={'class': 'form-controle', 'placeholder': 'optioneel',}),
-        }
+            'naam': forms.TextInput (attrs={'class': 'form-controle', 'placeholder': 'optioneel',}),}
 ######## // CONTACT FORMULIER #############
 
--- chelping/models.py
+++ chelping/models.py
@@ -3,30 +3,25 @@ from django.utils import timezone
 
 
 # CONTACTFORMULIER#
+
+NIV_CHOICES= [
+  ('0','basisschool'),
+  ('1','vmbo'),
+  ('2','havo'),
+  ('3','vwo'),
+]
+LEEFTIJD_KEUZE = [('5','5'),  ('6','6'),  ('17','17'),]
+TRAIN_KEUZE = [('RT','Remidial Teaching'),  ('LL','Leer Leren'),  ('NL', 'Nederlands'),('FA','Faalangst training')]
+
 class Contact(models.Model):
   naam = models.CharField('naam', max_length=50,  blank=False, null = True)
-  leeftijd = models.CharField(blank = False, max_length = 30, null = True,
-              choices=[
-                      ('basisschool', '6-8'),
-                      ('onderbouw', '9-12'),
-                      ('mavo', '13-14'),
-                      ('havo', '2011-2015'),])
-  niveau = models.CharField(max_length=30,default = 'Onbekend',blank=False, null = True,
-              choices=[
-                  ('basisschool', 'basis'),
-                  ('onderbouw', 'onderbouw'),
-                  ('mavo', 'mavo'),
-                  ('havo', 'havo'),
-                  ('vmbo', 'vmbo'),])
+  leeftijd = models.CharField(blank = False, max_length = 30, null = True,choices=LEEFTIJD_KEUZE)       
+  niveau = models.CharField(max_length=30,blank=False, choices=NIV_CHOICES, default = 'onbekend')
   email = models.EmailField('email',unique=False,   blank=False, null = True)
   telefoon = models.CharField('telefoon', max_length=30, blank = False, null = True)
   akkoord = models.BooleanField(default=False, null= False, blank = False)
-  training = models.CharField(max_length=30,default = 'Onbekend',blank=False, null = True,
-    choices=[
-        ('basisschool', 'Faalangst'),
-        ('onderbouw', 'Leer leren'),
-        ('mavo', 'Remedial Teaching'),
-         ('havo', 'Nederlands'),])
+  training = models.CharField(max_length=30,default = 'Onbekend',blank=False, null = True, choices=TRAIN_KEUZE)
+ 
   volgorde = models.PositiveIntegerField(default=0)
 
   def publish(self):
